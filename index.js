2e36eb737a9cf249d7de36bf90c81f01dbb0927f
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-16 18:03:11 +0000

    New functionnality: get user location

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 let map;
+let infoWindow;
 
 function initMap() {
     let mapOptions = {
     center: { lat: 41.881832, lng: -87.623177},
     zoom: 10
     };
-
     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement("button");
+
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+  
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+    
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("Location found.");
+              infoWindow.open(map);
+              map.setCenter(pos);
+            },
+            () => {
+              handleLocationError(true, infoWindow, map.getCenter());
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      });
+    //Defines then displays multiple markers on the map
     let markerList = [];
     let markerOptionsList = [ 
         {
