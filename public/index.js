884fe0ffb29fdbbb0ee0399c91014debc49ca709
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-18 09:19:54 +0000

    Added a location icon inside the location-btn and moved it to the right-bottom

--- public/index.js
+++ public/index.js
@@ -7,14 +7,18 @@ function initMap() {
     zoom: 8
   };
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  infoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow({
+    maxWidth: 350
+  });
 
   const locationButton = document.createElement("button");
+  const locationIcon = document.createElement("span");
+  locationButton.classList.add("user-location-btn");
+  locationIcon.classList.add("material-symbols-outlined", "user-location-icon");
+  locationIcon.textContent = "my_location";
+  locationButton.appendChild(locationIcon);
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(locationButton);
   locationButton.addEventListener("click", () => {
       // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -26,7 +30,7 @@ function initMap() {
           };
 
           infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
+          infoWindow.setContent("Votre position.");
           infoWindow.open(map);
           map.setCenter(pos);
         },
@@ -45,8 +49,8 @@ function initMap() {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
       browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
+        ? "Erreur: Le Service de Géolocatlisation a échoué."
+        : "Erreur: Votre navigateur ne supporte pas la géolocalisation."
     );
     infoWindow.open(map);
   }
@@ -58,7 +62,7 @@ function initMap() {
     {
         position: { lat: 6.6461292, lng: -4.7079362}, //Dimbokro
         map: map,
-        label: "Dimbokro"
+        title: "Dimbokro"
     },
     {
         position: { lat: 6.9282028, lng: -6.0334362}, //Bonon
@@ -67,21 +71,19 @@ function initMap() {
           scaledSize: new google.maps.Size(65, 50),
           url: './basketball-g0fea61622_1920-removebg-preview.png'
         },
-        label: "Utb express"
+        title: "Utb express"
     },
     {
         position: { lat: 5.8325039, lng: -5.3648169}, //Divo
         map: map,
-        label: "Utb-Divo"
+        title: "Utb-Divo"
     },
   ];
-  infoWindow = new google.maps.InfoWindow({
-  })
   for (let i = 0; i < markerOptionsList.length; i++) {
       markerList[i] = new google.maps.Marker( markerOptionsList[i] );
 
     markerList[i].addListener("click", () => {
-      infoWindow.setContent(getContentString(markerList[i].label));
+      infoWindow.setContent(getContentString(markerList[i].title));
       infoWindow.open({
         anchor: markerList[i],
         map,
@@ -102,13 +104,6 @@ function getContentString(label){
   '<h1 id="firstHeading" class="firstHeading">' + label + '</h1>' +
   '<div id="bodyContent">' +
   "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-  "sandstone rock formation in the southern part of the " +
-  "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-  "south west of the nearest large town, Alice Springs; 450&#160;km " +
-  "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-  "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-  "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-  "Aboriginal people of the area. It has many springs, waterholes, " +
   "rock caves and ancient paintings. Uluru is listed as a World " +
   "Heritage Site.</p>" +
   '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
