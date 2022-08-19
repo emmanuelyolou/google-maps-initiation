cfc891fbbbf3f0f17f57b783489e2332a8dfcec1
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-19 12:33:13 +0000

    the user position image is now visible

--- public/index.js
+++ public/index.js
@@ -70,6 +70,7 @@ function initMap() {
       super();
       this.pos = pos;
       this.image = image;
+      this.div = null;
     }
 
     /**
@@ -78,23 +79,31 @@ function initMap() {
    */
     onAdd() {
       this.div = document.createElement("div");
-      this.div.style.borderStyle = "none";
-      this.div.style.borderWidth = "0px";
+      this.div.classList.add("user-position-marker");
       this.div.style.position = "absolute";
-
+      
       // Create the img element and attach it to the div.
       const img = document.createElement("img");
 
       img.src = this.image;
-      img.style.width = "100%";
-      img.style.height = "100%";
       img.style.position = "absolute";
-      this.div.appendChild(img);
+
+      //Create a wrapper for the image to display the animation
+      let userMarkerImgWrapper = document.createElement('div');
+      userMarkerImgWrapper.classList.add('user-position-img-wrapper');
+      userMarkerImgWrapper.appendChild(img);
+      this.div.appendChild(userMarkerImgWrapper);
 
       // Add the element to the "overlayLayer" pane.
       const panes = this.getPanes();
-
       panes.overlayLayer.appendChild(this.div);
+    }
+
+    draw() {
+      const overlayProjection = this.getProjection();
+      const position = overlayProjection.fromLatLngToDivPixel( this.pos );
+      this.div.style.left = position.x + "px";
+      this.div.style.top = position.y + "px";
     }
 
     /**
@@ -137,100 +146,101 @@ function initMap() {
       }
     }
   }
-}
-
-
-function drop(){
-  clearMarkerList();
-  //The display of each marker is delayed in relation to the next marker
-  for (let i = 0; i < markerOptionsList.length; i++) {
-    setTimeout(() => {
-      markerList.push(
-        new google.maps.Marker({
-          position: markerOptionsList[i].position,
-          map,
-          animation: google.maps.Animation.DROP,
-        })
-      );
-      markerList[i].addListener("click", () => showLocationInfo(markerList[i], markerOptionsList[i]));
-    }, i * 300 );
-  }
-}
-
-function clearMarkerList() {
-  for (let i = 0; i < markerList.length; i++) {
-    markerList[i].setMap(null);
-  }
-  markerList = [];
-}
-
-function showLocationInfo(marker, markerOptions){
-  {
-    infoWindow.setContent(getContentString(markerOptions.title));
-    infoWindow.open({
-      anchor: marker,
-      map,
-      shouldFocus: false,
-    });
-  }
-}
-
-function showCurrentLocation(){
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-
-        // infoWindow.setPosition(pos);
-        // infoWindow.setContent("Votre position.");
-        // infoWindow.open(map);
-
-        // userLocationMarker.setPosition(pos);
-        // userLocationMarker.setMap(map);
-        userLocationMarker = new UserPositionOverlay(pos, "./images/user.png")
-        map.setCenter(pos);
-      },
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-//Fired if user refuses to give us access to his location
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Erreur: Le Service de Géolocatlisation a échoué."
-      : "Erreur: Votre navigateur ne supporte pas la géolocalisation."
-  );
-  infoWindow.open(map);
-}
-
   
-function getContentString(label){
-  return  '<div id="content">' +
-  '<div id="siteNotice">' +
-  "</div>" +
-  '<h1 id="firstHeading" class="firstHeading">' + label + '</h1>' +
-  '<div id="bodyContent">' +
-  "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-  "rock caves and ancient paintings. Uluru is listed as a World " +
-  "Heritage Site.</p>" +
-  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-  "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-  "(last visited June 22, 2009).</p>" +
-  "</div>" +
-  "</div>";
+  function drop(){
+    clearMarkerList();
+    //The display of each marker is delayed in relation to the next marker
+    for (let i = 0; i < markerOptionsList.length; i++) {
+      setTimeout(() => {
+        markerList.push(
+          new google.maps.Marker({
+            position: markerOptionsList[i].position,
+            map,
+            animation: google.maps.Animation.DROP,
+          })
+        );
+        markerList[i].addListener("click", () => showLocationInfo(markerList[i], markerOptionsList[i]));
+      }, i * 300 );
+    }
+  }
+
+  function clearMarkerList() {
+    for (let i = 0; i < markerList.length; i++) {
+      markerList[i].setMap(null);
+    }
+    markerList = [];
+  }
+
+  function showLocationInfo(marker, markerOptions){
+    {
+      infoWindow.setContent(getContentString(markerOptions.title));
+      infoWindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: false,
+      });
+    }
+  }
+
+  function showCurrentLocation(){
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          // infoWindow.setPosition(pos);
+          // infoWindow.setContent("Votre position.");
+          // infoWindow.open(map);
+
+          // userLocationMarker.setPosition(pos);
+          // userLocationMarker.setMap(map);
+          userLocationMarker = new UserPositionOverlay(pos, "./images/user.png")
+          userLocationMarker.setMap(map)
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
+
+  //Fired if user refuses to give us access to his location
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Erreur: Le Service de Géolocatlisation a échoué."
+        : "Erreur: Votre navigateur ne supporte pas la géolocalisation."
+    );
+    infoWindow.open(map);
+  }
+
+    
+  function getContentString(label){
+    return  '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">' + label + '</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+  }
+
 }
 
 
