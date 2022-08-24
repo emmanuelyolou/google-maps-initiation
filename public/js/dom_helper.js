fe3f13a23d324f69e864a04fe2bb86567cc4de53
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-24 17:15:53 +0000

    Rename of the temporary file into the DOMHelper class to provide helper functions with the DOM

---
+++ public/js/dom_helper.js
@@ -0,0 +1,44 @@
+export default class DOMHelper{
+    constructor(){};
+
+    createAccordion(){
+        //Wrapper of the whole accordion
+        let contentBox = document.createElement('div');
+        contentBox.classList.add('accordion-box');
+        //Title Of the accordion
+        let contentTitle = document.createElement('h5');
+        contentTitle.classList.add('accordion-content-title');
+        //content
+        let content = document.createElement('div');
+        content.classList.add('accordion-content');
+        //Info list (eg: distance, duration...)
+        let infoItemList = document.createElement('ul');
+        let infoItem = document.createElement('li');
+        let infoItemTitle = document.createElement('strong');
+        infoItemTitle.classList.add('info-item-title');
+        let infoItemValue = document.createElement('span');
+
+        infoItem.appendChild(infoItemTitle);
+        infoItem.appendChild(infoItemValue);
+        infoItemList.appendChild(infoItem);
+        content.appendChild(infoItemList);
+
+        contentBox.appendChild(contentTitle);
+        contentBox.appendChild(content);
+        return contentBox;
+    }
+
+    createInfoItem(title, value){
+        let infoItem = document.createElement('li');
+        let infoItemTitle = document.createElement('strong');
+        infoItemTitle.classList.add('info-item-title');
+        infoItemTitle.innerHTML = title;
+        let infoItemValue = document.createElement('span');
+        infoItemValue.innerHTML = value;
+
+        infoItem.appendChild(infoItemTitle);
+        infoItem.appendChild(infoItemValue);
+        debugger
+        return infoItem;
+    }
+}
